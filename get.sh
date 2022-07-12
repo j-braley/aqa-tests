9bf70bacc34e418123b52d610af10eedd0751413
Joe Braley <joebraley@microsoft.com>
2022-07-12 13:42:56 -0500

    Testing more ideas..

--- get.sh
+++ get.sh
@@ -385,8 +385,8 @@ getBinaryOpenjdk()
 					eval "ls -l $jar_name"
 					echo "Proceeding uncompressing file......."
 					unzip -q $jar_name -d ./tmp
-					echo "extract dir"
-					eval "ls -l $extract_dir"
+					echo "Test running java command....."
+					./tmp/bin/java.exe -version
 
 				elif [[ $jar_name == *.pax* ]]; then
 					cd ./tmp
@@ -614,11 +614,10 @@ testJavaVersion()
 	# 	# TEST_JDK_HOME=$SDKDIR/openjdkbinary/j2sdk-image
 	# 	TEST_JDK_HOME=/cygdrive/d/a/aqafer/aqafer/openjdkbinary/j2sdk-image
 	# fi
-	echo "$TEST_JDK_HOME"
 	TEST_JDK_HOME=/cygdrive/d/a/aqafer/aqafer/openjdkbinary/j2sdk-image
 	_java=${TEST_JDK_HOME}/bin/java
 	echo "Evaluating ${_java} path...."
-	# eval "ls -l D:/a/aqafer/aqafer/openjdkbinary/j2sdk-image/bin/java.exe"
+
 	_release=${TEST_JDK_HOME}/release
 	# Code_Coverage use different _java through searching javac for now, following path will be modified after refining files from BUILD
 	if [[ "$CODE_COVERAGE" == "true" ]]; then
